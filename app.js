8e549fad798943b41a0d9294824df5d1b268d1fe
Sullivan <sullivan.richard@outlook.com>
2020-04-12 20:08:13 +0200

    Clean Project Aquila

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ const app = express();
 
 app.use(serveStatic(path.join(__dirname, 'assets')));
 
-app.get('/login', function(req, res) {
-	res.sendFile(__dirname + '/login.html');
-});
-
-app.get('/admin', function(req, res) {
-	res.sendFile(__dirname + '/page.html');
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port);
